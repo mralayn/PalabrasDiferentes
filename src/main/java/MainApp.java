2fd9a66c002e33018b2b8e3146c99c37784e1d4f
mralayn <a215204767@unison.mx>
2022-02-02 15:53:53 -0700

    se agrego encontrar palabras diferentes.

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -3,11 +3,29 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class MainApp {
+    public static void ordenamientoArreglos(String[] palabras, int[] apariciones) {
+        for (int i = palabras.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (j + 1 <= i && apariciones[j] < apariciones[j + 1]) {
+                    String aux = palabras[j];
+                    int iux = apariciones[j];
+                    palabras[j] = palabras[j + 1];
+                    apariciones[j] = apariciones[j + 1];
+                    palabras[j + 1] = aux;
+                    apariciones[j + 1] = iux;
+                }
+            }
+        }
+    }
     static int palabrasTotales;
     public static void main(String args[]) {
         palabrasTotales =0;
+        Map<String, Integer> palabras = new HashMap();
 
         if (args.length == 0){
             System.out.println("falta el nombre del archivo");
@@ -40,8 +58,26 @@ public class MainApp {
             contador++;
             StringTokenizer st = new StringTokenizer(textLine);
             palabrasTotales = palabrasTotales + st.countTokens();
+            for (String palabra : textLine.replace(",", "").replace(".", "").replace(";", "").replace(":", "").split(" ")) {
+
+                palabras.put(palabra, palabras.containsKey(palabra) ? palabras.get(palabra) + 1 : 1);
+            }
 
         }
+
+        String[] arreglo_palabras = new String[palabras.size()];
+        int[] arreglo_repeticiones = new int[palabras.size()];
+        palabras.keySet().toArray(arreglo_palabras);
+        for (int i = 0; i < arreglo_palabras.length; i++) {
+            arreglo_repeticiones[i] = palabras.get(arreglo_palabras[i]);
+        }
+
+        ordenamientoArreglos(arreglo_palabras, arreglo_repeticiones);
+
+        for (int i = 0; i < arreglo_repeticiones.length; i++) {
+            System.out.println(arreglo_repeticiones[i] + " " + arreglo_palabras[i]);
+        }
+        System.out.println("Para este archivo de entrada, la palabra " + arreglo_palabras[0] + " ocurrió " + arreglo_repeticiones[0] + " veces, " + arreglo_palabras[1] + " " + arreglo_repeticiones[1] + " veces, etc.");
 
         try {
             in.close();
